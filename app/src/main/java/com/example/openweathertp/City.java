4a9e9d4735cf5fc0ad16aadcd795b82f597a9981
lheritier.adam <lheritier.adam@gmail.com>
2020-12-02 14:43:13 +0100

    classes, again

--- app/src/main/java/com/example/openweathertp/City.java
+++ app/src/main/java/com/example/openweathertp/City.java
@@ -1,4 +1,15 @@
 package com.example.openweathertp;
 
 public class City {
+    private int _id;
+    private String _name;
+    private Coord _coord;
+    private String _country;
+
+    public City(int _id, String _name, Coord _coord, String _country) {
+        this._id = _id;
+        this._name = _name;
+        this._coord = _coord;
+        this._country = _country;
+    }
 }
---
+++ app/src/main/java/com/example/openweathertp/Coord.java
@@ -0,0 +1,11 @@
+package com.example.openweathertp;
+
+public class Coord {
+    private double _lat;
+    private double _lon;
+
+    public Coord(double _lat, double _lon) {
+        this._lat = _lat;
+        this._lon = _lon;
+    }
+}
